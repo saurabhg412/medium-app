60afe43b9bef4748b95c6d4e5214bd2cca061830
saurabh gupta <saurabhgupta0122@gmail.com>
2024-03-08 20:38:17 +0530

    fixed password bug

--- common/dist/index.js
+++ common/dist/index.js
@@ -5,7 +5,7 @@ const zod_1 = require("zod");
 exports.signupSchema = zod_1.z.object({
     name: zod_1.z.string(),
     email: zod_1.z.string().email(),
-    password: zod_1.z.string()
+    password: zod_1.z.string().min(6),
 });
 exports.signinSchema = zod_1.z.object({
     email: zod_1.z.string().email(),
--- common/src/index.ts
+++ common/src/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const signupSchema = z.object({
     name : z.string(),
     email: z.string().email(),
-    password: z.string()
+    password: z.string().min(6),
 })
 
 export type signupSchema = z.infer<typeof signupSchema> 
