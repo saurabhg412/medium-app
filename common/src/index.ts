bb3c6e28e63473c45aa2af69f6c4f1e34b56aa1e
saurabh gupta <saurabhgupta0122@gmail.com>
2024-03-09 00:47:51 +0530

    updated schema

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
-    name : z.string(),
+    name : z.string().min(3).max(15),
     email: z.string().email(),
-    password: z.string().min(6),
+    password: z.string().min(6).max(10),
 })
 
 export type signupSchema = z.infer<typeof signupSchema> 
@@ -16,23 +16,22 @@ export const signinSchema = z.object({
 export type signinSchema = z.infer<typeof signinSchema>
 
 export const updatedUserSchema = z.object({
-    name : z.string().optional(),
-    email : z.string().email().optional(),
-    password : z.string().optional()
+    name : z.string().min(3).max(15).optional(),
+    password : z.string().min(6).max(10).optional()
 })
 
 export type updatedUserSchema = z.infer<typeof updatedUserSchema>
 
 export const postSchema = z.object({
-    title : z.string(),
-    content : z.string()
+    title : z.string().min(3).max(50),
+    content : z.string().max(2048)
 })
 
 export type postSchema = z.infer<typeof postSchema>
 
 export const updatedPostSchema = z.object({
-    title : z.string().optional(),
-    content : z.string().optional()
+    title : z.string().min(3).max(50).optional(),
+    content : z.string().max(2048).optional()
 })
 
 export type UpdatedPostSchema = z.infer<typeof updatedPostSchema>
